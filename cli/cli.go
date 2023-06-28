4f965c488472c7cd1b7098b1b5cc19d2d811b49c
Anton Novojilov <andy@essentialkaos.com>
2023-06-28 13:29:35 +0300

    Dependencies update

--- cli/cli.go
+++ cli/cli.go
@@ -295,6 +295,7 @@ func configureUI() {
 	progress.DefaultSettings.RemainingColorTag = ""
 	progress.DefaultSettings.BarFgColorTag = "{c}"
 	progress.DefaultSettings.IsSize = false
+	progress.DefaultSettings.WindowSizeSec = 60.0
 
 	fmtc.NameColor("package", "{m}")
 	fmtc.NameColor("repo", "{c}")
