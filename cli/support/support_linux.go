e95cf675dfc59cdd5a809d172243cf595c755a43
Anton Novojilov <andy@essentialkaos.com>
2023-12-05 13:24:45 +0300

    Code refactoring

--- cli/support/support_linux.go
+++ cli/support/support_linux.go
@@ -13,9 +13,9 @@ import (
 
 	"github.com/essentialkaos/ek/v12/fmtc"
 	"github.com/essentialkaos/ek/v12/fmtutil"
-	"github.com/essentialkaos/ek/v12/fsutil"
 	"github.com/essentialkaos/ek/v12/strutil"
 	"github.com/essentialkaos/ek/v12/system"
+	"github.com/essentialkaos/ek/v12/system/container"
 )
 
 // ////////////////////////////////////////////////////////////////////////////////// //
@@ -29,11 +29,12 @@ func showOSInfo() {
 
 		printInfo(12, "Name", osInfo.ColoredName())
 		printInfo(12, "Pretty Name", osInfo.ColoredPrettyName())
-		printInfo(12, "Version", osInfo.VersionID)
+		printInfo(12, "Version", osInfo.Version)
 		printInfo(12, "ID", osInfo.ID)
 		printInfo(12, "ID Like", osInfo.IDLike)
 		printInfo(12, "Version ID", osInfo.VersionID)
 		printInfo(12, "Version Code", osInfo.VersionCodename)
+		printInfo(12, "Platform ID", osInfo.PlatformID)
 		printInfo(12, "CPE", osInfo.CPEName)
 	}
 
@@ -53,11 +54,13 @@ func showOSInfo() {
 
 	containerEngine := "No"
 
-	switch {
-	case fsutil.IsExist("/.dockerenv"):
+	switch container.GetEngine() {
+	case container.DOCKER:
 		containerEngine = "Yes (Docker)"
-	case fsutil.IsExist("/run/.containerenv"):
+	case container.PODMAN:
 		containerEngine = "Yes (Podman)"
+	case container.LXC:
+		containerEngine = "Yes (LXC)"
 	}
 
 	fmtc.NewLine()
